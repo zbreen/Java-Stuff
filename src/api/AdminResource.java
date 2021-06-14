a90e57a5b69baced4880fde75e8dddd3cb8894b9
zbreen <43248045+zbreen@users.noreply.github.com>
2021-06-14 19:54:35 -0400

    Update AdminResource.java

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -7,19 +7,22 @@ import service.ReservationService;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.*;
 
 public class AdminResource {
     public static Customer getCustomer(String email){
         return CustomerService.getCustomer(email);
     }
     public static void addRoom(List<IRoom> rooms){
-
+        for(IRoom i: rooms){
+            ReservationService.addRoom(i);
+        }
     }
     public static Collection<IRoom> getAllRooms(){
-        return null;
+        return ReservationService.getAllRooms();
     }
     public static Collection<Customer> getAllCustomers(){
-        return null;
+        return CustomerService.getAllCustomers();
     }
     public static void displayAllReservations(){
         ReservationService.printAllReservation();
